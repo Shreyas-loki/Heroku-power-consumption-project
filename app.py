16dd9bdd4cab1af5bca59f8e77565417c0954857
Shreyas R <64091145+Shreyas-loki@users.noreply.github.com>
2021-08-11 12:40:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+app = Flask(__name__)
+model = pickle.load(open('household_power_consumption.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    """
+    For rendering results on HTML GUI
+    """
+    x_test = [float(x) for x in request.form.values()]
+    final_features = [np.array(x_test)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    
+    return render_template('index.html', prediction_text="Power consumed: {} kWh".format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
